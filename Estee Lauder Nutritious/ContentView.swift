2b46e4bc4c9873c1dd7d040a745ef1638aaebf7d
Tina Lin <lin547661@gmail.com>
2023-08-03 16:16:37 -0400

    Initial Commit

---
+++ Estee Lauder Nutritious/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Estee Lauder Nutritious
+//
+//  Created by Tina Lin on 8/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Estee Lauder Nutritious/Estee_Lauder_NutritiousApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_Lauder_NutritiousApp.swift
+//  Estee Lauder Nutritious
+//
+//  Created by Tina Lin on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_Lauder_NutritiousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
